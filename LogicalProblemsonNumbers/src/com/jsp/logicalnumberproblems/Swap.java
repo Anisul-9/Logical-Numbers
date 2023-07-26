edf5af46a18697f98874ee68bdba53db73ad8506
Anisul-9 <anisulabsar015@email.com>
2023-07-26 11:00:33 +0530

    change

--- LogicalProblemsonNumbers/src/com/jsp/logicalnumberproblems/Swap.java
+++ LogicalProblemsonNumbers/src/com/jsp/logicalnumberproblems/Swap.java
@@ -1,14 +1,10 @@
 package com.jsp.logicalnumberproblems;
 
 public class Swap {
-	public static void main(String[] args) {
-		int a=40;
-		int b=23;
-		
-		a=a-b;
-		b=a+a;
-		System.out.println(a);
-		System.out.println();
-	}
+	
+	int a=20;
+	int b=30;
+int temp;
+temp=a;
 
 }
